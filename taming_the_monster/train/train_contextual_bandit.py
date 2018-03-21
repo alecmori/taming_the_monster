3a67fc296ff75b2da19c68d2b9d19451d55a68e5
Alec Mori <alecmori@gmail.com>
2018-03-20 21:32:03 -0700

    Removed unnecessary filtering

--- taming_the_monster/train/train_contextual_bandit.py
+++ taming_the_monster/train/train_contextual_bandit.py
@@ -3,7 +3,6 @@ import staticconf
 
 from taming_the_monster import contextual_bandit_utils
 from taming_the_monster import data_iterator
-from taming_the_monster import filter_data
 from taming_the_monster import inverse_propensity_weighting
 from taming_the_monster import model
 
@@ -14,16 +13,12 @@ def main():
     staticconf.YamlConfiguration(CONFIG)
     contextual_bandit = []
     for training_data in data_iterator.iterate_data():
-        filtered_data = filter_data.filter_data(
-            training_data=training_data,
-            contextual_bandit=contextual_bandit,
-        )
         new_model = model.train_model(
-            X=filtered_data['chosen_actions'],
-            Y=filtered_data['rewards'],
+            X=training_data['chosen_actions'],
+            Y=training_data['rewards'],
             weights=inverse_propensity_weighting(
-                possible_actions=filtered_data['possible_actions'],
-                chosen_actions=filtered_data['chosen_actions'],
+                possible_actions=training_data['possible_actions'],
+                chosen_actions=training_data['chosen_actions'],
                 contextual_bandit=contextual_bandit,
             ),
         )
